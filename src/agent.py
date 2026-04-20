251f8994177f8fc6afb9d8f97dca664723590086
Maiko <ewigemagie@outlook.com>
2026-04-20 15:12:38 -0300

    feat: add better instructions, change to tradictional RAG for better context

--- src/agent.py
+++ src/agent.py
@@ -9,21 +9,26 @@ knowledge = Knowledge(
         collection="docs",
         path="tmp/chromadb",
         persistent_client=True,
-        search_type=SearchType.hybrid,
+        search_type=SearchType.vector,
         embedder=SentenceTransformerEmbedder(id="sentence-transformers/all-MiniLM-L6-v2")
     )
 )
 
-knowledge.insert(path='docs/')
+knowledge.insert(path='docs/', skip_if_exists=True)
 
 agent = Agent(
     model=LlamaCpp(
         id='ggml-org/gemma-3-1b-it-GGUF',
         base_url="http://localhost:8080/v1"
     ),
-    instructions="Write a detailed response of more than 250 words.",
+    instructions=[
+        "You are a specialized assistant for 'The Myriad Veil Cosmos'.",
+        "Always search the knowledge base for any information regarding sects, cultivation, characters, or lore.",
+        "If you find information in the knowledge base, prioritize it over your own training data.",
+        "Provide a detailed response of more than 250 words.",
+    ],
     markdown=True,
-    search_knowledge=True,
+    add_knowledge_to_context=True,
     knowledge=knowledge
 )
 
